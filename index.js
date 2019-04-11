a7d332d78a41c0b0bc08df7c9060ed9ffd70ca0d
Brandon <brandonbsuen@gmail.com>
2019-04-10 17:12:50 -0700

    Giving calculator its own inner-repository

---
+++ index.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import Head from 'next/head';
+
+import styles from './index.scss';
+
+export default class Calculator extends React.Component {
+	state = { display: '0', error: false };
+	//Animations on scroll, page title
+	_clear = () => this.setState({ display: '0' });
+	_onClickNum = num =>
+		this.setState({
+			display: this.state.display === '0' ? num : this.state.display + num
+		});
+	_onClickOp = op =>
+		this.setState({
+			display:
+				(this.state.display.endsWith(' ')
+					? this.state.display.substring(0, this.state.display.length - 3)
+					: this.state.display) +
+				' ' +
+				op +
+				' '
+		});
+	//Division by 0
+	//The IDs are so the tests are passed for FreeCodeCamp
+	render() {
+		return (
+			<div>
+				<Head>
+					<script src='https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js' />
+				</Head>
+				<div>
+					<section className={styles.calculator}>
+						<p className='display' id='display'>
+							{this.state.display}
+						</p>
+						<button
+							className={styles.button}
+							id='all-clear'
+							onClick={this._clear}
+						>
+							AC
+						</button>
+						<button className={styles.button} id='clear' onClick={this._clear}>
+							CE
+						</button>
+						<button
+							className={styles.button}
+							id='divide'
+							onClick={() => this._onClickOp('/')}
+						>
+							/
+						</button>
+						<button
+							className={styles.button}
+							id='multiply'
+							onClick={() => this._onClickOp('x')}
+						>
+							X
+						</button>
+						<button
+							className={styles.button}
+							id='seven'
+							onClick={() => this._onClickNum('7')}
+						>
+							7
+						</button>
+						<button
+							className={styles.button}
+							id='eight'
+							onClick={() => this._onClickNum('8')}
+						>
+							8
+						</button>
+						<button
+							className={styles.button}
+							id='nine'
+							onClick={() => this._onClickNum('9')}
+						>
+							9
+						</button>
+						<button
+							className={styles.button}
+							id='subtract'
+							onClick={() => this._onClickOp('-')}
+						>
+							-
+						</button>
+						<button
+							className={styles.button}
+							id='four'
+							onClick={() => this._onClickNum('4')}
+						>
+							4
+						</button>
+						<button
+							className={styles.button}
+							id='five'
+							onClick={() => this._onClickNum('5')}
+						>
+							5
+						</button>
+						<button
+							className={styles.button}
+							id='six'
+							onClick={() => this._onClickNum('6')}
+						>
+							6
+						</button>
+						<button
+							className={styles.button}
+							id='add'
+							onClick={() => this._onClickOp('+')}
+						>
+							+
+						</button>
+						<button
+							className={styles.button}
+							id='one'
+							onClick={() => this._onClickNum('1')}
+						>
+							1
+						</button>
+						<button
+							className={styles.button}
+							id='two'
+							onClick={() => this._onClickNum('2')}
+						>
+							2
+						</button>
+						<button
+							className={styles.button}
+							id='three'
+							onClick={() => this._onClickNum('3')}
+						>
+							3
+						</button>
+						<button
+							className={styles.button}
+							id='equals'
+							onClick={() => {
+								const evaluate = str => {
+									const terms = str.split(/ x | \/ /g);
+									console.log(terms);
+									terms[0] = parseFloat(terms[0]);
+									if (terms.length === 1) {
+										return terms[0];
+									} else {
+										const ops = str.replace(/[^(x|\/)]/g, '').split('');
+										console.log(ops);
+										return terms.reduce((a, b, q) => {
+											b = parseFloat(b);
+											if (ops[q - 1] === 'x') {
+												return a * b;
+											} else {
+												return a / b;
+											}
+										});
+									}
+								};
+								const terms = this.state.display.split(/ \+ | \- /g);
+								console.log(terms);
+								terms[0] = evaluate(terms[0]);
+								if (terms.length === 1) {
+									this.setState({ display: terms[0].toString() });
+								} else {
+									const ops = this.state.display
+										.replace(/[^(\+|\-)]/g, '')
+										.split('');
+									console.log(ops);
+									this.setState({
+										display: terms
+											.reduce((a, b, q) => {
+												if (b.length === 0) {
+													this.setState({ error: true });
+												} else {
+													b = evaluate(b);
+													if (ops[q - 1] === '+') {
+														return a + b;
+													} else {
+														return a - b;
+													}
+												}
+											})
+											.toString()
+									});
+								}
+							}}
+						>
+							=
+						</button>
+						<button
+							className={styles.button}
+							id='zero'
+							onClick={() => this._onClickNum('0')}
+						>
+							0
+						</button>
+						<button
+							className={styles.button}
+							id='decimal'
+							onClick={() =>
+								this.setState({
+									display:
+										(this.state.display.includes('.') &&
+											!this.state.display.includes(' ')) ||
+										(this.state.display.includes('.') &&
+											!this.state.display.includes(' ') &&
+											this.state.display.lastIndexOf('.') >
+												this.state.display.lastIndexOf(' '))
+											? this.state.display
+											: this.state.display + '.'
+								})
+							}
+						>
+							.
+						</button>
+					</section>
+				</div>
+			</div>
+		);
+	}
+}
